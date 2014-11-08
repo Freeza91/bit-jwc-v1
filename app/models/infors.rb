8e9dd061e7b70608fc999e49008c71d43997f432
hunter <347212291@qq.com>
2014-11-08 19:14:03 +0800

    update get infor

--- app/models/infors.rb
+++ app/models/infors.rb
@@ -1,73 +1,78 @@
 require 'net/http'
+require 'settings'
+require 'yaml'
+require 'uri'
 
 class Infors
 
-  def init(path, xml)
+  def self.init(path, xml)
     uri = URI Settings.host
     http = Net::HTTP.new(uri.host, uri.port)
     header = {'Content-Type' => 'text/xml; charset=utf-8'}
-    http.post(path, xml, header)
+    p xml
+    p http.post(path, xml, header)
   end
 
-  def login
+  def self.login
     xml = Settings.login_username + Settings.login_password + Settings.login_infor
     path = Settings.service_post
     res = init(path, xml)
     parse_login res
   end
 
-  def get_study_status(num)
-    xml = Settings.header + Settings.study_status% num + Settings.footer
+  def self.get_study_status(num)
+    xml = Settings.header + Settings.study_status % {:student_num => "#{num}"} + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
-    parse_study_status res
+    Tools.parse_study_status res
   end
 
-  def next_lesson(num)
-    xml = Settings.header + Settings.next_lesson% num + Settings.footer
+  def self.next_lesson(num)
+    xml = Settings.header + Settings.next_lesson % {:student_num => num} + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
-    parse_next_lesson res
+    Tools.parse_next_lesson res
   end
 
-  def exam(num)
-    xml = Settings.header + Settings.exam% num + Settings.footer
+  def self.exam(num)
+    xml = Settings.header + Settings.exam % {:student_num => num} + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
     parse_exam res
   end
 
-  def grade(num)
-    xml = Settings.header + Settings.grade% num + Settings.footer
+  def self.grade(num)
+    xml = Settings.header + Settings.grade % {:student_num => num} + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
     parse_grade res
   end
 
-  def today_list(num)
-    xml = Settings.header + Settings.today_list% num + Settings.footer
+  def self.today_list(num)
+    xml = Settings.header + Settings.today_list % {:student_num => num} + Settings.footer
     path = Settings.key_post
+
     res = init(path, xml)
     parse_today_list res
   end
 
-  def schedule_list(num)
-    xml = Settings.header + Settings.schedule_list% num + Settings.footer
+  def self.schedule_list(num)
+    xml = Settings.header + Settings.schedule_list % {:student_num => num} + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
     parse_schedule_list res
   end
 
-  def news_list
+  def self.news_list
     xml = Settings.header + Settings.news_list + Settings.footer
     path = Settings.key_post
     res = init(path, xml)
     parse_news_list res
   end
 
-  def school_tel
-    xml = Settings.header + Settings.schedule_list% 49753 + Settings.footer
-    path = Settings.tel_post
+  def self.school_tel
+    xml = Settings.header + Settings.schedule_list % {:student_num => 49753} + Settings.footer
+    path = Settings['tel_post']
     res = init(path, xml)
     parse_school_tel res
   end
---
+++ app/models/tools.rb
@@ -0,0 +1,45 @@
+require 'nokogiri'
+
+class Tools
+
+
+  def self.parse_login res
+    p res
+    doc = Nokogiri::XML.parse res.body
+    doc = doc.children.children.children #document-> Envelope-> Body p doc
+    p '-' * 30
+    doc = doc.children.children #loginverifystudent_BResponse -> loginverifystudent_BResult
+    p umcid = doc[2]
+    p contract = doc[6]
+  end
+
+  def self.parse_study_status res
+    doc = Nokogiri::XML.parse res.body
+
+  end
+
+  def self.parse_next_lesson res
+  end
+
+  def self.parse_exam res
+  end
+
+  def self.parse_grade res
+  end
+
+  def self.parse_today_list res
+  end
+
+  def self.parse_schedule_list res
+  end
+
+  def self.parse_news_list res
+  end
+
+  def self.parse_school_tel res
+  end
+
+  def is_right? status
+  end
+
+end
