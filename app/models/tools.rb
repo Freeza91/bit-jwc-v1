f42347d6dfd3cfb268587d127654e7cf2fb9b2b3
rudy <347212291@qq.com>
2014-11-13 13:42:39 +0800

    add infor to store database

--- app/models/tools.rb
+++ app/models/tools.rb
@@ -12,44 +12,46 @@ class Tools
   end
 
   def self.parse_study_status res
-    doc = Nokogiri::XML.parse res.body
-    doc = doc.children.children.children #document-> Envelope-> Body
-    doc = doc.children.children #loginverifystudent_BResponse -> StudyStateResultp
-    person = {}
-    p doc[0].children[0].text #姓名-key
-    p doc[0].children[1].text #姓名-value
-    person['username'] = doc[0].children[1].text
-    p '-' * 30
-    p doc[1].children[0].text #学号
-    p doc[1].children[1].text #
-    person['password'] = doc[1].children[1].text
-    p '-' * 30
-    p doc[2].children[0].text #性别
-    p doc[2].children[1].text #
-    p '-' * 30
-    p doc[3].children[0].text #学院
-    p doc[3].children[1].text #
-    p '-' * 30
-    p doc[4].children[0].text #专业
-    p doc[4].children[1].text #
-    p '-' * 30
-    p doc[5].children[0].text #班级
-    p doc[5].children[1].text #
-    p '-' * 30
-    p doc[6].children[0].text #入学时间
-    p doc[6].children[1].text #
-    p '-' * 30
-    p doc[7].children[0].text #学制
-    p doc[7].children[1].text #
-    p '-' * 30
-    p doc[8].children[0].text #学分呢情况
-    p doc[8].children[1].text #
-    p '-' * 30
-    p doc[9].children[0].text #仍未通过课程
-    p doc[9].children[1].text #
-    p '-' * 30
-    p doc[10].children[0].text #曾经不及格课程总学分
-    p doc[10].children[1].text #
+    begin
+      doc = Nokogiri::XML.parse res.body
+      doc = doc.children.children.children #document-> Envelope-> Body
+      doc = doc.children.children #loginverifystudent_BResponse -> StudyStateResultp
+      person = {}
+      p doc[0].children[1].text #姓名-value
+      person['username'] = doc[0].children[1].text
+      p doc[1].children[1].text #学号
+      person['password'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[2].children[0].text #
+      p doc[2].children[1].text #性别
+      person['sex'] = doc[2].children[1].text
+      p '-' * 30
+      p doc[3].children[1].text #学制院
+      person['academy'] = doc[3].children[1].text
+      p '-' * 30
+      p doc[4].children[1].text #专业
+      person['profession'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[5].children[1].text #班级
+      person['class_num'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[6].children[1].text #入学时间
+      person['begin_time'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[7].children[1].text #学制
+      person['period'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[8].children[1].text #学分情况
+      person['grade'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[9].children[1].text #仍未通过课程
+      person['no_grade_record'] = doc[1].children[1].text
+      p '-' * 30
+      p doc[10].children[1].text #曾经不及格课程总学分
+      person['no_grade'] = doc[1].children[1].text
+    rescue Exception
+    else
+    end
     person
   end
 
@@ -58,12 +60,17 @@ class Tools
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #NexttermyLessonResponse -> NexttermyLessonResult
     p doc.size
-    doc.each do |e|
-    sub = e.children
-      p sub[0].text #课程代码
-      p sub[1].text #课程名字
+    lession = {}
+    begin
+      doc.each do |e|
+        sub = e.children
+        p sub[0].text #课程代码
+        p sub[1].text #课程名字
+        lession[sub[0].text] = sub[1].text
+      end
+    rescue Exception
     end
-
+    lession
   end
 
   def self.parse_exam res
@@ -71,11 +78,17 @@ class Tools
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #ExamResponse -> ExamResult
     p doc.size
-    doc.each do |e|
-      sub = e.children
-      p sub[0].text #考试名字
-      p sub[1].text #考试时间和地点
+    exam = {}
+    begin
+      doc.each do |e|
+        sub = e.children
+        p sub[0].text #考试名字
+        p sub[1].text #考试时间和地点
+        exam[sub[0].text] = sub[1].text
+      end
+    rescue Exception
     end
+    exam
   end
 
   def self.parse_grade res
@@ -83,21 +96,34 @@ class Tools
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #MarkResponse -> MarkResult
     p doc.size
-    doc.each do |e|
-      sub = e.children
-      p sub[0].text #科目
-      p sub[1].text #成绩
+    grade = {}
+    begin
+      doc.each do |e|
+        sub = e.children
+        p sub[0].text #科目
+        p sub[1].text #成绩
+        grade[sub[0].text] = sub[1].text
+      end
+    rescue Exception
     end
+    grade
   end
 
   def self.parse_today_list res
     doc = Nokogiri::XML.parse res.body
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #getDeptPhoneByPage_SResponse -> getDeptPhoneByPage_SResult
-    doc.each do |e|
-      sub = e.children
-      p sub.text unless sub.nil? || sub.text.size.zero? #列表
+    list = []
+    begin
+      doc.each do |e|
+        sub = e.children
+        unless sub.nil? || sub.text.size.zero? #列表
+          list << sub.text
+        end
+      end
+    rescue Exception
     end
+    list
   end
 
   def self.parse_schedule_list res
@@ -106,22 +132,36 @@ class Tools
     doc = doc.children.children #GetScheduleListResponse -> GetScheduleListResult
     p doc.size
     i = 0
-    doc.each do |e|
-      i += 1
-      sub = e.children
-      if i > 14 && !sub[0].nil?
-        p sub[2].text #开始上课小节
-        p sub[3].text #课程名字
-        p sub[4].text #老师
-        p sub[5].text #地点
-        p sub[6].text #星期
-        p sub[7].text #上课时间
-        p sub[9].text #班号
-        p sub[10].text#班名字
-        p sub[11].text#上课周期
-        p "*" * 30
+    schedule_list = {}
+    begin
+      doc.each do |e|
+        i += 1
+        sub = e.children
+        if i > 14 && !sub[0].nil?
+          p sub[2].text #开始上课小节
+          schedule_list['start_at_num'] = sub[2].text
+          p sub[3].text #课程名字
+          schedule_list['subject_name'] = sub[3].text
+          p sub[4].text #老师
+          schedule_list['teacher'] = sub[4].text
+          p sub[5].text #地点
+          schedule_list['place'] = sub[5].text
+          p sub[6].text #星期
+          schedule_list['week'] = sub[6].text
+          p sub[7].text #上课时间
+          schedule_list['start_at_time'] = sub[7].text
+          p sub[9].text #班号
+          schedule_list['bid'] = sub[9].text
+          p sub[10].text#班名字
+          schedule_list['bname'] = sub[10].text
+          p sub[11].text#上课周期
+          schedule_list['period'] = sub[11].text
+          p "*" * 30
+        end
       end
+    rescue Exception
     end
+    schedule_list
   end
 
   def self.parse_news_list res
@@ -129,24 +169,40 @@ class Tools
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #GetNewsListResponse -> GetNewsListResult
     p doc.size
-    doc.each do |e|
-      sub = e.children
-      p sub[1].text #类别
-      p sub[3].text #标题
-      p sub[6].text #时间
-      p sub[7].text #简介
-      p sub[8].text #详情地址url
-      p "*" * 30
+    news = {}
+    begin
+      doc.each do |e|
+        sub = e.children
+        p sub[1].text #类别
+        news['category'] = sub[1].text
+        p sub[3].text #标题
+        news['title'] = sub[3].text
+        p sub[6].text #时间
+        news['time'] = sub[6].text
+        p sub[7].text #简介
+        news['profile'] = sub[7].text
+        p sub[8].text #详情地址url
+        news['url'] = sub[8].text
+        p "*" * 30
+      end
+    rescue Exception
     end
+    news
   end
 
   def self.parse_school_tel res
     doc = Nokogiri::XML.parse res.body
     doc = doc.children.children.children #document-> Envelope-> Body
     doc = doc.children.children #getDeptPhoneByPage_SResponse -> getDeptPhoneByPage_SResult
-    doc.each do |e|
-      sub = e.children
-      p sub.text unless sub.nil? || sub.text.size.zero? #电话
+    tel = []
+    begin
+      doc.each do |e|
+        sub = e.children
+        unless sub.nil? || sub.text.size.zero? #电话
+          tel << sub.text
+        end
+      end
+    rescue Exception
     end
   end
 
