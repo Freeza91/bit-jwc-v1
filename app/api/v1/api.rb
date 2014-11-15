9758ee2d2f826dc31c29449f9b39722618b3bda9
hunter <347212291@qq.com>
2014-11-15 23:56:59 +0800

    add weixin init

--- app/api/v1/api.rb
+++ app/api/v1/api.rb
@@ -2,17 +2,18 @@ require 'grape-swagger'
 
 module V1
   class API < Grape::API
-    format :json
+    format :xml
+    content_type :xml, "text/xml"
     version :v1
 
-    get '/' do
+    get '/auth' do
       "hello"
     end
 
     get '/example' do
-      "example"
+      #'<example>"example"<example>'
     end
 
-    add_swagger_documentation mount_path: 'doc.json', base_path: 'api', api_version: 'v1'
+    add_swagger_documentation mount_path: 'doc.xml', base_path: 'api', api_version: 'v1'
   end
 end
---
+++ app/api/v1/xml_helpers.rb
@@ -0,0 +1,15 @@
+require 'nokogiri'
+
+module V1
+  module XmlHelpers
+    def text_reply
+    end
+
+    def image_reply
+    end
+
+    def article_reply
+    end
+
+  end
+end
