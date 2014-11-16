8ec41e4459e1124425cb5053302fbc93f3e5adbb
hunter <347212291@qq.com>
2014-11-16 12:53:16 +0800

    add week_schedule

---
+++ app/models/week_schedule.rb
@@ -0,0 +1,9 @@
+class WeekSchedule
+  include Mongoid::Document
+
+  field :start_at_num, type: Integer
+  field :classroom,    type: String
+
+  validates :start_at_num, presence: true
+  validates :classroom,    presence: true
+end
