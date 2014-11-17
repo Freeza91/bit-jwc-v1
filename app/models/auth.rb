08d2743f676e2b86f9e40a074b1cff8dc76143b6
hunter <347212291@qq.com>
2014-11-17 22:00:24 +0800

    add login online bit

--- app/models/auth.rb
+++ app/models/auth.rb
@@ -2,17 +2,63 @@ require 'net/https'
 require 'uri'
 require 'nokogiri'
 require 'open-uri'
+require 'curb'
 
 class Auth
-  def login(username, password)
-    index = Setting.login_webiste
+
+  def initialize
+    @curl = Curl::Easy.new
+    @curl.enable_cookies = true
+    @curl.cookiefile = '/tmp/online'
+    @curl.cookiejar = '/tmp/online'
+    @curl.follow_location = true
+    @curl.useragent = 'Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/38.0.2125.111 Safari/537.36'
+    @tries = 3;
   end
 
-  def download_image cookie
+  def login(username, password)
+    @curl.url = Setting.login_webiste
+    begin
+      @curl.get
+    rescue
+      false
+    end
+    @curl.cookiefile = '/tmp/online'
+    doc = Nokogiri::HTML(@curl.body)
+    params = {}
+    begin
+      params['username'] = username
+      params['password'] = password
+      params['lt'] = doc.css('input[name="lt"]')[0]['value']
+      params['execution'] = doc.css('input[name="execution"]')[0]['value']
+      params['_eventId'] = doc.css('input[name="_eventId"]')[0]['value']
+      params['rmShown'] = doc.css('input[name="rmShown"]')[0]['value']
+    rescue
+      if @tries != 0
+        @tries -= 1
+        return login(username, password)
+      end
+      false
+    end
+
+    form_fields = URI.encode_www_form(params)
+    begin
+      @curl.http_post(form_fields)
+    rescue
+      false
+    end
+
+    if @curl.response_code == 200
+      @curl.body.force_encoding('UTF-8')
+    end
+    false
+  end
+
+  def download_image
     index = Settings.profile
     uri = URI index
     req = Net::HTTP::Get.new uri
-    req['Cookie'] = cookie
+    req['Cookie'] = @curl.cookie
     res = Net::HTTP.start(uri.hostname, uri.port) do |http|
       http.request req
     end
